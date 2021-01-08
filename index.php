9815e2ca7989476c9dc22212c06efa8200b01abb
Akinbande <47974658+Akinbande@users.noreply.github.com>
2021-01-08 01:19:10 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- Begin Head -->
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description"  content=""/>
+	<meta name="keywords" content=""/>
+	<meta name="author"  content=""/>
+	<meta name="MobileOptimized" content="320" />
+	<title>Atom Energy Storage Systems</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="assets/css/fonts.cdcss" />
+	<link rel="stylesheet" href="assets/css/fontawesome.min.css" />
+	<link rel="stylesheet" href="assets/css/magnific-popup.css" />
+	<link rel="stylesheet" href="assets/css/swiper.min.css" />
+	<link rel="stylesheet" href="assets/css/animate.css" />
+	<link rel="stylesheet" href="assets/css/style.css" />
+	<link rel="stylesheet" href="assets/css/color3.css" />
+	<!--=== Favicon Link ===-->
+	<link rel="shortcut icon" type="image/png" href="assets/images/fav-icon.png">
+</head>
+<body class="index_three">
+<section class="ss_section_one"> <!--===== Section One Start =====-->
+	<div class="container-fluid">
+		<div class="ss_header white_header"> <!--===== Top Menu =====-->
+			<div class="row">
+				<div class="col-lg-3 col-md-7 col-sm-7 col-7 align-self-center">
+					<div class="ss_logo">
+						<a href="index.html"><img class="img-fluid" src="assets/images/svg/black_logo.png" alt="logo" /></a>
+					</div>
+				</div>
+				<div class="col-lg-9 col-md-5 col-sm-5 col-5 align-self-center">
+					<div class="ss_menu ss_menu_two">			 
+					 
+				</div>
+			</div>
+		</div>
+		<div class="ss_banner_main banner_three"> <!--===== Banner =====-->
+			<div class="row">
+				<div class="col-xl-5 col-lg-6 align-self-center">
+					<div class="ss_banner_left">
+						<h1>Reliable Energy Storage Solutions Company</h1>
+						<a href="#about" class="ss_btn violet_btn">About Us</a>
+					</div>
+				</div>
+				<div class="col-xl-7 col-lg-6">
+					<div class="ss_banner_right">
+						<img class="img-fluid" src="assets/images/img1.png" alt="image"/>
+					</div>
+				</div>
+			</div>
+			<div class="solar_waves">
+				<div class="solar_wave"></div>
+				<div class="solar_wave"></div>
+				<div class="solar_wave"></div>
+			</div>
+			<div class="shape_dot">
+				<img class="img-fluid" src="assets/images/shape_dot.png" alt="image"/>
+			</div>
+			<div class="shape_wave2">
+				<img class="img-fluid" src="assets/images/wave.png" alt="image"/>
+			</div>
+		</div>
+	</div>
+</section> <!--===== Section One End =====-->
+<section class="ss_section_two spacer_top"> <!--===== Section Two Start =====-->
+	<div class="container-fluid">
+		<div class="ss_two"> 
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="ss_two_sec wow fadeIn" data-wow-delay="0.1s">
+						<img class="img-fluid" src="assets/images/svg/icon1.svg" alt="image"/>
+						<h2>Atom Solar</h2>
+						<p>Our solution provides low-cost, carbon-free on demand power for households and small business</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="ss_two_sec wow fadeIn" data-wow-delay="0.1s">
+						<img class="img-fluid" src="assets/images/svg/icon4.svg" alt="image"/>
+						<h2>Digital Smarter</h2>
+						<p>AtomEss has developed one of the most advanced BMS with active and passive equalizer with a real-time control on every single cell.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="ss_two_sec wow fadeIn" data-wow-delay="0.1s">
+						<img class="img-fluid" src="assets/images/svg/icon3.svg" alt="image"/>
+						<h2>Power Monitoring</h2>
+						<p>We offer on-demand remote monitoring and provides complete connectivity for operating efficiency of solar assets.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="ss_two_sec wow fadeIn" data-wow-delay="0.1s">
+						<img class="img-fluid" src="assets/images/svg/icon2.svg" alt="image"/>
+						<h2>Quality Design</h2>
+						<p>Design quality in every detail, first-class assistance worldwide from our skilled technicians</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section> <!--===== Section Two End =====-->
+
+<section id="about">
+<section class="ss_section_three spacer_bottom"> <!--===== Section Three Start =====-->
+	<div class="container-fluid"> 
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="ss_three_left">
+					<img class="img-fluid" src="assets/images/smile .jpg" alt="image"/>
+				</div>
+			</div>
+			<div class="col-lg-6 align-self-center">
+				<div class="ss_three_right wow fadeIn" data-wow-delay="0.1s">
+					<h3>best service</h3>
+					<h1>We are Building a Sustainable Products</h1>
+					<p>Atom ESS is a manufacturer of Lithium Ion based batteries. We are providing cost competitive energy storage products that very affordable and perfectly fit for the Emerging market.	</p>
+					<p class="pb-4">We are among the world’s leading manufacturer of Lithium Ion based batteries energy solutions provider that is revolutionising and redefining the way sustainable 
+						energy sources are harnessed across the world. </p>
+					<a href="service.html" class="ss_btn">Know More</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section> <!--===== Section Three End =====-->
+</section>
+
+<section class="ss_section_five ss_section_sec_bg spacer_top spacer_bottom"> <!--===== Section Five Start =====-->
+	<div class="container-fluid"> 
+		<h3 class="ss_h3_center text-center">easy to atom</h3>
+		<h1 class="text-center mb-5">We are the Best-In-Class Products & Solutions</h1>
+		<div class="row"> 
+			<div class="col-xl-4 col-lg-6">
+				<div class="ss_four_left wow fadeIn" data-wow-delay="0.1s">
+					<div class="ss_box_bg">
+						<div class="row">
+							<div class="col-3">
+								<div class="ss_four_one">
+									<img class="img-fluid" src="assets/images/svg/icon1.svg" alt="image"/>
+								</div>
+							</div>
+							<div class="col-9">
+								<div class="ss_four_two">
+									<h2>Best Energy Solutions</h2>
+									<p>Over two decades of rich experience in the wind energy sector. Over 17,900 MW capacity.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="ss_box_bg mt-4">
+						<div class="row">
+							<div class="col-3">
+								<div class="ss_four_one">
+									<img class="img-fluid" src="assets/images/svg/icon1.svg" alt="image"/>
+								</div>
+							</div>
+							<div class="col-9">
+								<div class="ss_four_two">
+									<h2>End-To-End Services</h2>
+									<p>Vast experience in power evacuation, land pro curement, liaisoning and working with state.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="ss_box_bg mt-4">
+						<div class="row">
+							<div class="col-3">
+								<div class="ss_four_one">
+									<img class="img-fluid" src="assets/images/svg/icon1.svg" alt="image"/>
+								</div>
+							</div>
+							<div class="col-9">
+								<div class="ss_four_two">
+									<h2>Global Expertise</h2>
+									<p>A perfect blend of global experience and expertise to further our focus on technology. advancement.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-4 col-lg-6 order-xl-last mt-lg-0 mt-md-4">
+				<div class="ss_four_left wow fadeIn" data-wow-delay="0.1s">
+					<div class="ss_box_bg">
+						<div class="row">
+							<div class="col-3">
+								<div class="ss_four_one">
+									<img class="img-fluid" src="assets/images/svg/icon3.svg" alt="image"/>
+								</div>
+							</div>
+							<div class="col-9">
+								<div class="ss_four_two">
+									<h2>Long Time to Use</h2>
+									<p> You enjoy the our services, and we offer remote monitoring and predictive maintenance services through our system. </p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="ss_box_bg mt-4">
+						<div class="row">
+							<div class="col-3">
+								<div class="ss_four_one">
+									<img class="img-fluid" src="assets/images/svg/icon3.svg" alt="image"/>
+								</div>
+							</div>
+							<div class="col-9">
+								<div class="ss_four_two">
+									<h2>Renewable Energy</h2>
+									<p>We provide clean and reliable power for productive use to all business</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="ss_box_bg mt-4">
+						<div class="row">
+							<div class="col-3">
+								<div class="ss_four_one">
+									<img class="img-fluid" src="assets/images/svg/icon3.svg" alt="image"/>
+								</div>
+							</div>
+							<div class="col-9">
+								<div class="ss_four_two">
+									<h2>Power Monitoring</h2>
+									<p>We offer on-demand remote monitoring and provides complete connectivity for operating efficiency of solar assets.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-4 offset-xl-0 col-lg-6 offset-lg-3">
+				<div class="ss_easy_use wow zoomIn" data-wow-delay="0.2s">
+					<img class="img-fluid" src="assets/images/battrey.png" alt="image"/>
+				</div>
+			</div>
+		</div>
+	</div>
+</section> <!--===== Section Five End =====-->
+
+<section class="ss_section_four spacer_top spacer_bottom"> <!--===== Section Four Start =====-->
+	<div class="container-fluid"> 
+		<div class="row">
+			<div class="col-lg-6 align-self-center">
+				<div class="ss_five_right wow fadeIn" data-wow-delay="0.1s">
+					<h3>Our Mission</h3>
+					<h1>We are here to give you the best energy storage solution</h1>
+					<p>We are mission-driven to put Africa on course to a sustainable energy by providing market fit storage systems</p>					 
+				</div>
+			</div>
+			<div class="col-lg-6 align-self-center">
+				<div class="ss_four_right">
+					<img class="img-fluid ss_four_shadow" src="assets/images/mission.jpg" alt="image" title="image" />
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</section> <!--===== Section Four End =====-->
+
+<section class="ss_newsletter ss_section_sec_bg"> <!--===== Section Newsletter Start =====-->
+	<div class="container-fluid">
+		<div class="ss_footer_news ss_box_bg">	
+			<div class="row"> 
+				<div class="col-md-7">
+					<div class="ss_foot_news_one">
+						<h1>Stay connected with us</h1>
+						<p>You can also drop us a mail at hello@atom-ess.com</p>
+					</div>
+				</div> 
+				<div class="col-md-5 align-self-center">
+					<div class="ss_foot_news_one">
+						<input type="text" placeholder="Enter your email" />
+						<button class="ss_btn">Submit</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section> <!--===== Section Newsletter End =====-->
+
+<section class="ss_section_footer"> <!--===== Section Footer Start =====-->
+	<div class="container-fluid">	
+		<div class="row"> 
+			<div class="col-lg-5 col-md-12">
+				<div class="row"> 				 
+			</div>
+			<div class="col-lg-7 col-md-12">
+			 
+			</div>
+		</div>
+	</div>
+</section> <!--===== Section Footer End =====-->
+<div class="ss_copywrite">
+	<p>&copy Copyright 2021, All right reserved by <a href="index.html">Atom Energy Storage Systems</a></p>
+</div>
+
+<!--===== script start =====-->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/isotope.pkgd.min.js"></script>
+<script src="assets/js/swiper.min.js"></script>
+<script src="assets/js/wow.min.js"></script>
+<script src="assets/js/script.js"></script>
+	
+</body>
+</html>
